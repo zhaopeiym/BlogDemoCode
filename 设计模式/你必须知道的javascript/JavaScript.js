2997b9a994f46daaaa57ce0f452d253c2c93bae4
benny <zhaopeiym@163.com>
2017-03-19 19:32:31 +0800

    设计模式

---
+++ 设计模式/你必须知道的javascript/JavaScript.js
@@ -0,0 +1,25 @@
+﻿var 鸭子 = {
+    走路: function () {
+        console.log("走路咯");
+    },
+    咕咕咕: function () { }
+}
+
+var 鸟 = {
+    走路: function () {
+        console.log("走路咯");
+    },
+    咕咕咕: function () { }
+}
+
+var 鸭子们 = [];
+鸭子们.push(鸭子);
+鸭子们.push(鸟);
+
+for (var i = 0; i < 鸭子们.length; i++) {
+    鸭子们[i].走路();
+}
+
+
+
+
