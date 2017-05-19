fde72a55067e936110410a57f92d65c206cab0a5
benny <zhaopeiym@163.com>
2017-05-19 17:50:43 +0800

    AOP

---
+++ 切面编程/AOP/Demo/DemoClass.cs
@@ -0,0 +1,41 @@
+﻿using Castle.DynamicProxy;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo
+{
+    public class MyClass : IMyClass
+    {
+        public virtual void MyMethod()
+        {
+            Console.WriteLine("My Mehod");
+        }
+    }
+
+    public class TestIntercept : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine("111111111111");
+            invocation.Proceed();
+            Console.WriteLine("222");
+        }
+    }
+    
+    public class Test_tests
+    {
+        [Fact]
+        public void tests()
+        {
+            var proxyGenerate = new ProxyGenerator();
+            TestIntercept t = new TestIntercept(); 
+            var pg = proxyGenerate.CreateClassProxy<MyClass>(t);
+            pg.MyMethod(); 
+        }
+    } 
+}
---
+++ 切面编程/AOP/Demo/IMyClass.cs
@@ -0,0 +1,6 @@
+﻿namespace Demo
+{
+    public interface IMyClass
+    {
+    }
+}
