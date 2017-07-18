f62a82b49c416f60a84da51aab278fbb8561f386
benny <zhaopeiym@163.com>
2017-07-18 17:56:11 +0800

    上传demo

---
+++ 上传下载/上传下载/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 上传下载.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult SaveFile1()
+        {              
+            return View(Request.Files.Count.ToString());
+        }
+
+        public string SaveFile2()
+        {
+            return Request.Files.Count.ToString();
+        }
+    }
+}
---
+++ 上传下载/上传下载/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="上传下载.MvcApplication" Language="C#" %>
