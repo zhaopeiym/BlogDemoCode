a2476b954e0b8d669747b0add0ea68e23d6da07d
benny <zhaopeiym@163.com>
2017-07-21 18:01:55 +0800

    上传

--- 上传下载/上传下载/Controllers/HomeController.cs
+++ 上传下载/上传下载/Controllers/HomeController.cs
@@ -1,41 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.IO;
 using System.Web.Mvc;
 
 namespace 上传下载.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-
         public ActionResult SaveFile1()
-        {              
+        {
             return View(Request.Files.Count.ToString());
         }
 
         public string SaveFile2()
         {
+            Request.SaveAs(@"D:\test.png", false);
             return Request.Files.Count.ToString();
         }
+
+        public string SaveFile3()
+        {
+            using (FileStream fs = new FileStream(@"D:\test.data", FileMode.Append, FileAccess.Write))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    BinaryReader br = new BinaryReader(Request.InputStream);
+                    bw.Write(br.ReadBytes((int)Request.InputStream.Length));
+                } 
+            }          
+            return Request.Files.Count.ToString();
+        }
+
+        ///// <summary>
+        ///// 流转化为字节数组
+        ///// </summary>
+        ///// <param name="stream">流</param>
+        ///// <returns>字节数组</returns>
+        //public static byte[] StreamToByte(Stream stream)
+        //{            
+        //    using (MemoryStream tempStream = new MemoryStream())
+        //    {
+        //        int bi;
+        //        while ((bi = stream.ReadByte()) != -1)
+        //        {
+        //            tempStream.WriteByte(((byte)bi));
+        //        }
+        //        return tempStream.ToArray();
+        //    };
+        //}
     }
 }
