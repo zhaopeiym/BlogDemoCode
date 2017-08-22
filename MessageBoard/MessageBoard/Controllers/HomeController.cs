df306ae1bb62c6cc19c25b9bd953b9b8197a0593
benny <zhaopeiym@163.com>
2017-08-22 14:47:57 +0800

    dev

--- MessageBoard/MessageBoard/Controllers/HomeController.cs
+++ MessageBoard/MessageBoard/Controllers/HomeController.cs
@@ -18,24 +18,35 @@ namespace MessageBoard.Controllers
         }
         public async Task<IActionResult> Index(int page, int cont)
         {
+            HttpContext.SetToken();
+            ViewBag.Token = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss").DES3Encrypt("talkantimaliceXX");
             ViewBag.Messgs = await GetData(0, 30);
             return View();
         }
 
-
         public async Task<List<Message>> GetData(int page, int cont)
         {
             cont = cont == 0 || cont > 30 ? 30 : cont;
-
             var messgs = await _db.Messages.OrderByDescending(t => t.CreateTime).Skip(page).Take(cont).AsNoTracking().ToListAsync();
             return messgs;
         }
 
         [HttpPost]
-        public async Task<string> RecordMessges(string msg, string userName)
+        public async Task<string> RecordMessges(string msg, string userName, string token)
         {
-            if (!HttpContext.GetChance(10))
+            try
+            {
+                var time = Des.DesDecrypt("javascriptjsencr", token);
+                time = time.DES3Decrypt("talkantimaliceXX");
+                if (DateTime.Parse(time).AddMinutes(3) <= DateTime.Now)//三分钟内需要提交
+                    return "正加载中...";
+                if (!HttpContext.GetChance(10))
+                    return "正加载中...";
+            }
+            catch (Exception)
+            {
                 return "正加载中...";
+            }
             if (!string.IsNullOrEmpty(msg))
             {
                 var ip = GetUserIp();
---
+++ MessageBoard/MessageBoard/EncryptDecryptExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBoard
+{
+    /// <summary>
+    /// 加密、解密
+    /// </summary>
+    public static class EncryptDecryptExtension
+    {
+        // 3DES 加密解密
+
+        /// <summary>
+        /// 必须16位
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string DES3Encrypt(this string data, string key)
+        {
+
+            byte[] inputArray = Encoding.UTF8.GetBytes(data);
+            var tripleDES = TripleDES.Create();
+            var byteKey = Encoding.UTF8.GetBytes(key);
+            byte[] allKey = new byte[24];
+            Buffer.BlockCopy(byteKey, 0, allKey, 0, 16);
+            Buffer.BlockCopy(byteKey, 0, allKey, 16, 8);
+            tripleDES.Key = allKey;
+            tripleDES.Mode = CipherMode.ECB;
+            tripleDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = tripleDES.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        public static string DES3Decrypt(this string data, string key)
+        {
+            byte[] inputArray = Convert.FromBase64String(data);
+            var tripleDES = TripleDES.Create();
+            var byteKey = Encoding.UTF8.GetBytes(key);
+            byte[] allKey = new byte[24];
+            Buffer.BlockCopy(byteKey, 0, allKey, 0, 16);
+            Buffer.BlockCopy(byteKey, 0, allKey, 16, 8);
+            tripleDES.Key = allKey;
+            tripleDES.Mode = CipherMode.ECB;
+            tripleDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = tripleDES.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
