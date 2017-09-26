4fad536ceeb98683377b8c82d311aca4b0973aba
benny <zhaopeiym@163.com>
2017-09-26 15:52:04 +0800

    /为了验证实体跟踪是否会占用内存不是释放,而没有使用AsNoTracking

---
+++ EFCoreDemo/Migrations/20170926064542_db2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EFCoreDemo.Migrations
+{
+    public partial class db2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Blogs");
+        }
+    }
+}
